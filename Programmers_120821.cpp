76ef5e610f205246a6eef3c7421b7ad3e2465bd9
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-01-25 00:58:17 +0900

    120821번 : 배열 뒤집기

---
+++ Programmers_120821.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    int iSize = num_list.size();
+    for(int i=iSize-1;i>=0;--i)
+    {
+        answer.push_back(num_list[i]);
+    }
+    return answer;
+}
